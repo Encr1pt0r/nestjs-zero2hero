d1587fef28ecfea5ab99098fb38e2e094ef9ff47
Encr1pt0r <presidor16@gmail.com>
2024-04-02 08:54:24 +0100

    configation management with joi

---
+++ src/config.schema.ts
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().required(),
+  DB_HOST: Joi.number().default(5432).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+});
